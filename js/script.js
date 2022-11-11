f7fb9a3d3ea03dac96e6370beb356426a94f8674
Stefania De Flaviis <stefania.deflaviis@gmail.com>
2022-11-11 15:01:33 +0100

    print a list of items thanks to an API

---
+++ js/script.js
@@ -0,0 +1,36 @@
+// **Descrizione:**
+// Attraverso l’apposita API di Boolean
+// https://flynn.boolean.careers/exercises/api/random/mail
+// generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
+// **Bonus**
+// Creare un loading e far comparire gli indirizzi email solamente quando sono stati TUTTI generati.
+
+const {createApp} = Vue;
+
+createApp({
+  data() {
+    return {
+      apiUrl: 'https://flynn.boolean.careers/exercises/api/random/mail',
+      MailList: [],
+      numberOfItems: 10,
+    }
+  },
+  methods: {
+    getApi() {
+      axios.get(`${this.apiUrl}`)
+        .then ( risultato => {
+          this.MailList.push(risultato.data.response);
+        })
+    },
+
+    generateList(number) {
+      for (i = 0; i < number; i++) {
+        this.getApi();
+      }
+    }
+
+  },
+  mounted() {
+    this.generateList(this.numberOfItems);
+  }
+}).mount('#app');
